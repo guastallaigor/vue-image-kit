ae2fcbf7e37a00e58841d9f7037271923ac8cf3b
guastallaigor <iguastalla@uolinc.com>
2020-11-26 12:56:22 -0300

    chore: add missing dev-dependencies, update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   overrides: [
     {
       files: [
-        "**/tests/unit/*.spec.{j,t}s?(x)"
+        '**/tests/unit/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
