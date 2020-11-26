d4f7f70a49a59dcc761c78f7ce24d4559f0f2050
guastallaigor <iguastalla@uolinc.com>
2020-11-26 12:52:28 -0300

    chore: update project dependencies, storybook, readme

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  logLevel: 'debug',
+  addons: [
+    '@storybook/addon-controls',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y'
+  ]
+}
--- vue-image-kit.js
+++ vue-image-kit.js
@@ -1,22 +1,39 @@
+// Import vue component
 import VueImageKit from './src/components/VueImageKit.vue'
 
-// Export components
-const Components = {
-  VueImageKit
+// install function executed by Vue.use()
+const install = function (Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('VueImageKit', VueImageKit)
 }
 
-const VueImageKitPlugin = {
-  install (Vue) {
-    Object.keys(Components).forEach((name) => {
-      Vue.component(name, Components[name])
-    })
-  }
+// Create module definition for Vue.use()
+const plugin = {
+  install
 }
 
-// Export as a plugin
-export default VueImageKitPlugin
+// To auto-install when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+let GlobalVue = null
 
-// Export as individual components
-export {
-  VueImageKit
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
 }
+
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+VueImageKit.install = install
+
+// Export component by default
+export default VueImageKit
+
+// Export single (backwards compatibility)
+export { VueImageKit }
